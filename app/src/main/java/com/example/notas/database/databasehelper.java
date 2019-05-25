eb65858013f3f19269b5c0c2fcc803931e7169b6
ErubielFlota <santosflota8@gmail.com>
2019-05-24 21:41:43 -0500

    llegue a agregar las notas de insercion

--- app/src/main/java/com/example/notas/database/databasehelper.java
+++ app/src/main/java/com/example/notas/database/databasehelper.java
@@ -1,4 +1,29 @@
 package com.example.notas.database;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class databasehelper {
+import com.example.notas.database.model.Nota;
+
+import java.security.PrivateKey;
+
+public class databasehelper extends SQLiteOpenHelper {
+    private  static final int DATABASE_VERSION=1;
+    private static final String DATABASE_NAME= "notes_db";
+
+    public databasehelper(Context context){
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(Nota.CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){
+        db.execSQL("DROP TABLE IF EXISTS "+ Nota.TABLE_NAME);
+
+        onCreate(db);
+    }
+
 }
